8a64089b4b18d91afa20382a8338322d8daf79b1
1101331 <1101331@est.intec.edu.do>
2021-07-08 18:05:20 -0400

    Estructura de clases establecida

---
+++ src/Transaction.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+
+namespace CajaEmeute
+{
+    class Session
+    {
+        private string username;
+        private string password;
+        private string connexion;
+        private TransactionBuffer buffer;
+
+        private DateTime sessionStart;
+
+        public Session(string _username, string _password, string _connexion)
+        {
+            buffer = new TransactionBuffer(20);
+            username = _username;
+            password = _password; //Storing password in plain text !!TEMP MEASURE!!!
+            connexion = _connexion;
+            //add log here for conn status
+            sessionStart = DateTime.Now;
+
+            
+        }
+
+        public void CreateTransaction(Transaction t)
+        {
+            buffer.AddTransactionToBuffer(t);
+        }
+
+        public bool CheckConnection()
+        {
+            bool conn = true; //replace by function to check connection to api!!!!
+            if (conn)
+                return true;
+            return false;
+        }
+
+        private bool logIn()
+        {
+            //Add code to send username and password
+            return true;
+        }
+
+
+
+    }
+
+    class TransactionBuffer
+    {
+        private Queue<Transaction> data; //Temporal replacement for acutal formatted data !!CHANGE SOON!!
+        public TransactionBuffer(int bufferLength)
+        {
+            data = new Queue<Transaction>();
+        }
+
+        public void loadTextBuffer() //Loads buffered transactions save to text
+        {
+            //add log here
+        }
+
+        public void AddTransactionToBuffer(Transaction t)
+        {
+            data.Enqueue(t);
+            //add log here
+        }
+    }
+
+    class Transaction //placeholder members !!change according to core!!
+    {
+        private DateTime issueDate;
+        private String pacientID;
+        private String operationType;
+        private int monetaryAmount;
+
+        public Transaction(String _pacientID, String _operationType, int _monetaryAmount)
+        {
+            issueDate = DateTime.Now;
+            pacientID = _pacientID;
+            operationType = _operationType;
+            monetaryAmount = _monetaryAmount;
+        }
+    }
+}
